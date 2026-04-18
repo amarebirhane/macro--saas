140e9c7caf9c68607ccb601fe7c7dbbd53879bba
amarebirhane <amarebirhane89@gmail.com>
2026-04-18 12:20:07 +0300

    updated backend

---
+++ backend/app/core/email.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+from typing import Any, Dict
+
+from fastapi_mail import ConnectionConfig, FastMail, MessageSchema, MessageType
+from app.core.config import settings
+
+conf = ConnectionConfig(
+    MAIL_USERNAME=settings.SMTP_USER,
+    MAIL_PASSWORD=settings.SMTP_PASSWORD,
+    MAIL_FROM=settings.EMAILS_FROM_EMAIL,
+    MAIL_PORT=settings.SMTP_PORT,
+    MAIL_SERVER=settings.SMTP_HOST,
+    MAIL_FROM_NAME=settings.EMAILS_FROM_NAME,
+    MAIL_STARTTLS=settings.SMTP_TLS,
+    MAIL_SSL_TLS=not settings.SMTP_TLS, # Usually one or the other
+    USE_CREDENTIALS=True,
+    VALIDATE_CERTS=True,
+    TEMPLATE_FOLDER=Path(__file__).parent.parent / "templates",
+)
+
+fastmail = FastMail(conf)
+
+async def send_email(
+    email_to: str,
+    subject: str,
+    template_name: str,
+    template_body: Dict[str, Any],
+) -> None:
+    message = MessageSchema(
+        subject=subject,
+        recipients=[email_to],
+        template_body=template_body,
+        subtype=MessageType.html,
+    )
+    await fastmail.send_message(message, template_name=template_name)
