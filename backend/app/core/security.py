f8c146b30e1bd7c9fc19bf2d19b4000717b3f749
amarebirhane <amarebirhane89@gmail.com>
2026-04-18 04:04:27 +0300

    updated some features

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,1 +1,16 @@
+from datetime import datetime, timedelta, timezone
+from typing import Any, Union
+from jose import jwt
+from app.core.config import settings
 from app.utils.hashing import verify_password, get_password_hash
+
+def create_access_token(subject: Union[str, Any], role: str, expires_delta: timedelta = None) -> str:
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(
+            minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+        )
+    to_encode = {"exp": expire, "sub": str(subject), "role": role}
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    return encoded_jwt
